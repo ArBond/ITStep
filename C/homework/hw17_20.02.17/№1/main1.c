b01bfb40419146c4e6748816be76e1f100b4196c
ArBond <bonartembon@gmail.com>
2017-02-26 18:25:36 +0300

    added hw17_20.02.17 on C

---
+++ C/homework/hw17_20.02.17/№1/main1.c
@@ -0,0 +1,34 @@
+﻿/* Задание 1:
+   Напишите программу поиска факториала числа без использования возвращения значения
+   из функции факториала(return result;), используйте указатель на переменную с
+   результатом и передавайте ее в рекурсивную функцию(например, factor(int* result)) */
+
+
+#include <stdio.h>
+
+void calculateFactorial(int number, double* result);
+
+int main()
+{
+	printf("Davajte poschitaem faktorial.\nVvedite chislo(do 22): ");
+	int number;
+	scanf("%i", &number);
+	while (number < 0 || number > 22)
+	{
+		printf(number < 1 ? "Faktorial otricatel'nogochisla ne opredelen\n":"Slishkom bol'shoe chislo\n");
+		scanf("%i", &number);
+	}
+	double result = 1;
+	calculateFactorial(number, &result);
+	printf("Faktorial '%i' = %.0lf\n", number, result);
+	return 0;
+}
+
+void calculateFactorial(int number, double* result)
+{
+	if (number > 1)
+	{
+		calculateFactorial(number - 1, result);
+		*result *= number;
+	}
+}
---
+++ C/homework/hw17_20.02.17/№2/main2.c
@@ -0,0 +1,46 @@
+﻿/* Задание 2:
+   Напишите программу возведения чисел в степень, используйте указатели для передачи
+   результирующего значения в функцию и из нее.Числа необходимо получать от
+   пользователя. */
+
+
+#include <stdio.h>
+
+void findNumber(int number, int degree, double* result);
+
+int main()
+{
+	printf("Vozvedem chislo v stepen'!\nVvedite chislo: ");
+	int number;
+	scanf("%i", &number);
+	while (number > 99 || number < -99)
+	{
+		printf(number > 99 ? "Slishkom bol'shoe chislo\n":"Slishkom malen'koe chislo\n");
+		scanf("%i", &number);
+	}
+	printf("Stepen': ");
+	int degree;
+	scanf("%i", &degree);
+	while (degree > 9 || degree < 0)
+	{
+		printf(degree > 9 ? "Slishkom bol'shaja stepen'\n" : "Vozvedenije v oticatel'nuju stepen' ne podderzhivaetsa programmoj\n");
+		scanf("%i", &degree);
+	}
+	double result = number;
+	findNumber(number, degree, &result);
+	printf("%i ^ %i = %.0lf\n", number, degree, result);
+	return 0;
+}
+
+void findNumber(int number, int degree, double* result)
+{
+	if ((number > 1 || number < 0) && degree > 1)
+	{
+		findNumber(number, degree - 1, result);
+		*result *= number;
+	}
+	if (degree == 0 && number != 0)
+	{
+		*result = 1;
+	}
+}
