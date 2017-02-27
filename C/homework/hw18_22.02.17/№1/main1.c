c45a7353f8911af1a6e0b35e65b48b87cfb70d28
ArBond <bonartembon@gmail.com>
2017-02-27 21:49:22 +0300

    added hw18_22.02.17 on C

---
+++ C/homework/hw18_22.02.17/№1/main1.c
@@ -0,0 +1,54 @@
+﻿/* Задание 1:
+   Используя два указателя на массив целых чисел, скопировать один массив в другой.
+   Использовать в программе арифметику указателей для продвижения по массиву, а также
+   оператор разыменования. */
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define ARRAY_SIZE 9
+
+void fillArray(int* array, int* arraySize);
+void printArray(int* array, int* arraySize);
+void copyArray(int* startArray, int* finalArray, int* arraySize);
+
+int main()
+{
+	srand(time(0));
+	int startArray[ARRAY_SIZE];
+	int arraySize = ARRAY_SIZE;
+	fillArray(&startArray, &arraySize);
+	printf("Vash Massiv:\n");
+	printArray(&startArray, &arraySize);
+	int finalArray[ARRAY_SIZE];
+	copyArray(&startArray, &finalArray, &arraySize);
+	printf("\nCopiruem...\n\nVash novyj massiv:\n");
+	printArray(&finalArray, &arraySize);
+	return 0;
+}
+
+void fillArray(int* array, int* arraySize)
+{
+	for (int i = 0; i < *arraySize; i++)
+	{
+		*(array + i) = rand() % 10;
+	}
+}
+
+void printArray(int* array, int* arraySize)
+{
+	for (int i = 0; i < *arraySize; i++)
+	{
+		printf("#%i = %i\n", i + 1, *(array + i));
+	}
+}
+
+void copyArray(int* startArray, int* finalArray, int* arraySize)
+{
+	for (int i = 0; i < *arraySize; i++)
+	{
+		*(finalArray + i) = *(startArray + i);
+	}
+}
---
+++ C/homework/hw18_22.02.17/№2/main2.c
@@ -0,0 +1,55 @@
+﻿/* Задание 2:
+   Используя указатель на массив целых чисел, изменить порядок следования элементов
+   массива на противоположный.Использовать в программе арифметику указателей для
+   продвижения по массиву, а также оператор разыменования. */
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define ARRAY_SIZE 9
+
+void fillArray(int* array, int* arraySize);
+void printArray(int* array, int* arraySize);
+void reverseArray(int* array, int* arraySize);
+
+int main()
+{
+	srand(time(0));
+	int array[ARRAY_SIZE];
+	int arraySize = ARRAY_SIZE;
+	fillArray(&array, &arraySize);
+	printf("Vash Massiv:\n");
+	printArray(&array, &arraySize);
+	reverseArray(&array, &arraySize);
+	printf("\nReversiruem massiv...\n\nVot chto poluchilos:\n");
+	printArray(&array, &arraySize);
+	return 0;
+}
+
+void fillArray(int* array, int* arraySize)
+{
+	for (int i = 0; i < *arraySize; i++)
+	{
+		*(array + i) = rand() % 10;
+	}
+}
+
+void printArray(int* array, int* arraySize)
+{
+	for (int i = 0; i < *arraySize; i++)
+	{
+		printf("#%i = %i\n", i + 1, *(array + i));
+	}
+}
+
+void reverseArray(int* array, int* arraySize)
+{
+	for (int i = 0; i < *arraySize / 2; i++)
+	{
+		int buffer = *(array + i);
+		*(array + i) = *(array + *arraySize - 1 - i);
+		*(array + *arraySize - 1 - i) = buffer;
+	}
+}
