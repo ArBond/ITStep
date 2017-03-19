9c242c853489e56708e1e5ad0d3bc1216c10b442
ArBond <bonartembon@gmail.com>
2017-03-19 12:16:21 +0300

    added hw22_13.03.17

---
+++ C/homework/hw22_13.03.17/№1/main1.c
@@ -0,0 +1,61 @@
+﻿/*	Задание 1:
+	Написать функцию, которая вставляет в строку в указанную позицию заданный символ */
+
+
+#include <stdio.h>
+
+int findLengthOfString(char* string);
+void incertSymbol(char* string, char userSymbol, int placeForIncert, int stringLength);
+void clearChar();
+
+void main()
+{
+	char string[] = "This is your string in which you can to insert a symbol.\n";
+	printf("String: %s", string);
+	int stringLength = findLengthOfString(string);
+	printf("What symbol do you want to insert: ");
+	char userSymbol;
+	do
+	{
+		scanf("%c", &userSymbol);
+	} while (userSymbol == '\n');
+	clearChar();
+	printf("In what place do you want to incert (1 - %i): ", stringLength);
+	int placeForIncert;
+	scanf("%i", &placeForIncert);
+	while (placeForIncert > stringLength || placeForIncert < 1)
+	{
+		printf(placeForIncert < 1 ? "The symbol number can't to be less then 1.\n" : "String contains only %i symbols.\n", stringLength);
+		scanf("%i", &placeForIncert);
+	}
+	incertSymbol(string, userSymbol, placeForIncert, stringLength);
+	printf("New string: %s\n", string);
+}
+
+int findLengthOfString(char* string)
+{
+	int stringLength = 0;
+	while(*(string + stringLength) != 0 && *(string + stringLength) != '\n')
+	{
+		stringLength++;
+	}
+	return stringLength;
+}
+
+void incertSymbol(char* string, char userSymbol, int placeForIncert, int stringLength)
+{
+	for (int i = stringLength; i > placeForIncert - 1; i--)
+	{
+		*(string + i) = *(string + i - 1);
+	}
+	*(string + placeForIncert - 1) = userSymbol;
+}
+
+void clearChar()
+{
+	char ch;
+	do
+	{
+		ch = getchar();
+	} while (ch != '\n' && ch != EOF);
+}
---
+++ C/homework/hw22_13.03.17/№2/main2.c
@@ -0,0 +1,45 @@
+﻿/*	Задание 1:
+	Написать функцию, которая определяет, явдяется ли введенная строка анаграммой */
+
+
+#include <stdio.h>
+
+#define ARRAY_SIZE 51
+
+void isAnagram(char* userString, int stringLengh);
+int findStringLength(char* userString);
+
+void main()
+{
+	char userString[ARRAY_SIZE];
+	printf("Enter the string(up to %i symbols): ", ARRAY_SIZE - 1);
+	do
+	{
+		fgets(userString, ARRAY_SIZE, stdin);
+	} while (*userString == '\n');
+	printf("Your string: %s", userString);
+	int stringLengh = findStringLength(userString);
+	isAnagram(userString, stringLengh);
+}
+
+int findStringLength(char* userString)
+{
+	int stringLengh = 0;
+	while (userString[stringLengh] != '\n' && userString[stringLengh] != 0)
+	{
+		stringLengh++;
+	}
+	return stringLengh;
+}
+
+void isAnagram(char* userString, int stringLengh)
+{
+	int count = 0;
+	while (userString[count] == userString[stringLengh - 1 - count] || 
+		userString[count] >= 'a' && userString[count] <= 'z' && userString[count] == userString[stringLengh - 1 - count] + 32 ||
+		userString[count] >= 'A' && userString[count] <= 'Z' && userString[count] == userString[stringLengh - 1 - count] - 32)
+	{
+		count++;
+	}
+	printf(count == stringLengh ? "It's an anagram.\n" : "It's not an anagram.\n");
+}
