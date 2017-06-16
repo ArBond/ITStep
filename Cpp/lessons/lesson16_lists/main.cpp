8d3253a18d735281b1e252f85113db0b5e50ece0
ArBond <bonartembon@gmail.com>
2017-06-16 11:58:36 +0300

    Create main.cpp

---
+++ Cpp/lessons/lesson16_lists/main.cpp
@@ -0,0 +1,49 @@
+#include "list.h"
+
+#include <iostream>
+#include <list>
+#include <algorithm>
+
+void main()
+{
+
+	//		LIST 1
+
+	//List<int> list = { -1, -4 };
+	//list.insert(4, list.end());
+	//list.insert(5, list.end());
+	//list.insert(2, list.begin());
+	//list.insert(10, list.begin(-9));
+	//list.insert(10, list.begin(100));
+	//list.erase(list.begin(1));
+
+	//for (List<int>::iterator it = list.begin(); it != list.end(); ++it)
+	//{
+	//	std::cout << *it << " ";
+	//}
+	//std::cout << "\ncount: " << list.getCount() << "\n";
+
+
+	//// standart templates
+
+	//std::list<int> list3 = { 123, 23, 23, 454 };
+	//for (std::list<int>::iterator it = list3.begin(); it != list3.end(); ++it)
+	//{
+	//	std::cout << *it << " ";
+	//}
+	//std::cout << "\n";
+
+	//std::list<int>::iterator it1 =
+	//	std::find(list3.begin(), list3.end(), 23);
+	//if (it1 != list3.end())
+	//{
+	//	std::cout << "found " << *it1 << "\n";
+	//}
+	//std::cout << "count " << std::count(list3.begin(), list3.end(), 23);
+
+
+	//		LIST2
+
+
+
+}
