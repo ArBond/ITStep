d9eb570aa88075a5bec4f5b0a531814fabf43236
ArBond <bonartembon@gmail.com>
2017-09-20 16:10:14 +0300

    move lesson7

---
+++ Cs/lessons/lesson7_complex-operators overloading/operators overloading/StrPoint.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lesson7
+{
+    public struct StrPoint
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public StrPoint(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+    }
+}
