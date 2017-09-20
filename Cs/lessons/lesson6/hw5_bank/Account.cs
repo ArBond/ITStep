47c18636d1d8c56f3c50d9dc2544a55d18d7c9c5
ArBond <bonartembon@gmail.com>
2017-09-20 13:25:56 +0300

    Add files via upload

---
+++ Cs/lessons/lesson6/hw5_bank/Account.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hw5_19._09._17
+{
+    class Account
+    {
+        private CreditCard CreditCard;
+        private string Name;
+
+        private decimal Balance { get; set; }
+    }
+}
