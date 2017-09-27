eb5396b37c0ecb008b5caa45e84cfa13fe9c7532
ArBond <bonartembon@gmail.com>
2017-09-27 13:17:39 +0300

    Create program.cs

---
+++ Cs/lessons/lesson11_extensions and containers/frequency analysis/program.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lesson11_List
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new WordCounter().Work();
+        }
+    }
+}
