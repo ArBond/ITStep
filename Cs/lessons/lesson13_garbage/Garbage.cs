0e091e7b8806fdd80ee3126fc1038ebc3a00915e
ArBond <bonartembon@gmail.com>
2017-10-02 11:46:05 +0300

    Add files via upload

---
+++ Cs/lessons/lesson13_garbage/Garbage.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lesson13
+{
+    public class Garbage: IDisposable
+    {
+        private int[] array = new int[100000];
+
+        public void Dispose()
+        {
+            Console.WriteLine("Dispozed!");
+        }
+    }
+}
