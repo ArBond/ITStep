1c31a83751e346586b973165004e190d4a36b4dd
ArBond <bonartembon@gmail.com>
2017-10-09 11:53:08 +0300

    Add files via upload

---
+++ Cs/lessons/lesson17_serializing-attributes/attributes/AuthorAttribute.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace attributes
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = true)]
+    public class AuthorAttribute: Attribute
+    {
+        public string Name { get; private set; }
+        public DateTime Date { get; private set; }
+
+        public AuthorAttribute(string name, string date)
+        {
+            Name = name;
+            DateTime parsedDate;
+            if (DateTime.TryParse(date, out parsedDate))
+                Date = parsedDate;
+            else
+                Date = DateTime.Now;
+        }
+    }
+}
---
+++ Cs/lessons/lesson17_serializing-attributes/attributes/Student.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace attributes
+{
+    [Author("Artemi", "09.10.17")]
+    public class Student
+    {
+        public string FullName { get; set; }
+
+        public DateTime BirthDate { get; set; }
+
+        [Author("Artemi", "09.10.17")]
+        public void Learn(string knowlage)
+        {
+            throw new NotSupportedException();
+        }
+
+        [Author("Artemi", "09.10.17")]
+        [Author("Alex", "10.10.17")]
+        public void DoNothing()
+        {
+        }
+    }
+}
