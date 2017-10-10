70c3360bda1441269b1c2cf7683efc162fa85ae8
ArBond <bonartembon@gmail.com>
2017-10-10 11:49:44 +0300

    Add files via upload

---
+++ Cs/lessons/lesson18_using legacy code/exam/City.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamCS_PilotSimulator
+{
+    public class City
+    {
+        public string Name { get; set; }
+
+        public City(string name)
+        {
+            Name = name;
+        }
+    }
+}
---
+++ Cs/lessons/lesson18_using legacy code/exam/Flight.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamCS_PilotSimulator
+{
+    public class Flight
+    {
+        public City StartCity { get; set; } = new City("Minsk");
+        public City FinishCity { get; set; }
+        public Pilot Pilot { get; set; }
+        public Plane Plane { get; set; } = new Plane("777");
+        public List<Dispatcher> Dispatchers { get; set; } = new List<Dispatcher>();
+
+        public Flight(Pilot pilot, City finishCity)
+        {
+            Pilot = pilot;
+            FinishCity = finishCity;
+        }
+
+        public void AddDispatcher(Dispatcher dispatcher)
+        {
+            Dispatchers.Add(dispatcher);
+        }
+
+        public void DeleteDispatcher(Dispatcher dispatcher)
+        {
+            if (Dispatchers.Count > 2)
+                Dispatchers.Remove(dispatcher);
+            else
+                Console.WriteLine();
+        }
+    }
+}
