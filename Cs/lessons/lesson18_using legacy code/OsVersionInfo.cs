c8603ef2c37b1b70fc1db2a8e2abc2460d6bafc2
ArBond <bonartembon@gmail.com>
2017-10-10 10:39:37 +0300

    Add files via upload

---
+++ Cs/lessons/lesson18_using legacy code/OsVersionInfo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lesson18
+{
+    public unsafe struct OsVersionInfo
+    {
+        public uint size;
+        public uint majorVersion;
+        public uint minorVersion;
+        public uint buildNumber;
+        public uint platformId;
+        public fixed byte servicePackVersion[128];
+    }
+}
---
+++ Cs/lessons/lesson18_using legacy code/WinApiHelper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lesson18
+{
+    public static class WinApiHelper
+    {
+        [DllImport("User32.dll", EntryPoint = "MessageBoxW", CharSet = CharSet.Auto)]
+        public static extern int ShowMessageBox(IntPtr hWnd, string text, string caption, uint type);
+
+        [DllImport("Kernel32.dll")]
+        public static extern bool GetVersionEx(ref OsVersionInfo info);
+
+        [DllImport("Advapi32.dll")]
+        public static extern bool GetUserName(StringBuilder name, ref int size);
+    }
+}
