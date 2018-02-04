4c95646f16e35f6270264718d1d25915413045f7
ArBond <bonartembon@gmail.com>
2018-02-04 20:40:05 +0300

    my video conference(video only)

---
+++ Cs- web programming/homeworks/hw1_25.01.18/hw1_25.01.18_new/Form1.Designer.cs
@@ -0,0 +1,232 @@
+﻿using System.Windows.Forms;
+
+namespace hw1_25._01._18_new
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        private void InitializeComponent()
+        {
+            this.friendsPictureBox = new System.Windows.Forms.PictureBox();
+            this.MyPictureBox = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.IPAddressTextBox = new System.Windows.Forms.TextBox();
+            this.LocalPortTextBox = new System.Windows.Forms.TextBox();
+            this.RemotePortTextBox = new System.Windows.Forms.TextBox();
+            this.ChatTextBox = new System.Windows.Forms.TextBox();
+            this.SendingMessageTextBox = new System.Windows.Forms.TextBox();
+            this.ConnectButton = new System.Windows.Forms.Button();
+            this.SendMessageButton = new System.Windows.Forms.Button();
+            this.VideoDevicesComboBox = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.StartCameraButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.friendsPictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.MyPictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // friendsPictureBox
+            // 
+            this.friendsPictureBox.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.friendsPictureBox.Location = new System.Drawing.Point(20, 12);
+            this.friendsPictureBox.Name = "friendsPictureBox";
+            this.friendsPictureBox.Size = new System.Drawing.Size(454, 321);
+            this.friendsPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.friendsPictureBox.TabIndex = 0;
+            this.friendsPictureBox.TabStop = false;
+            // 
+            // MyPictureBox
+            // 
+            this.MyPictureBox.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.MyPictureBox.Location = new System.Drawing.Point(494, 12);
+            this.MyPictureBox.Name = "MyPictureBox";
+            this.MyPictureBox.Size = new System.Drawing.Size(131, 88);
+            this.MyPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.MyPictureBox.TabIndex = 1;
+            this.MyPictureBox.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(512, 138);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(89, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "IP address:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(512, 260);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(102, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Remote port:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(518, 199);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(83, 20);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Local port:";
+            // 
+            // IPAddressTextBox
+            // 
+            this.IPAddressTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.IPAddressTextBox.Location = new System.Drawing.Point(494, 161);
+            this.IPAddressTextBox.Name = "IPAddressTextBox";
+            this.IPAddressTextBox.Size = new System.Drawing.Size(131, 26);
+            this.IPAddressTextBox.TabIndex = 5;
+            // 
+            // LocalPortTextBox
+            // 
+            this.LocalPortTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.LocalPortTextBox.Location = new System.Drawing.Point(494, 222);
+            this.LocalPortTextBox.Name = "LocalPortTextBox";
+            this.LocalPortTextBox.Size = new System.Drawing.Size(131, 26);
+            this.LocalPortTextBox.TabIndex = 6;
+            // 
+            // RemotePortTextBox
+            // 
+            this.RemotePortTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.RemotePortTextBox.Location = new System.Drawing.Point(494, 283);
+            this.RemotePortTextBox.Name = "RemotePortTextBox";
+            this.RemotePortTextBox.Size = new System.Drawing.Size(131, 26);
+            this.RemotePortTextBox.TabIndex = 7;
+            // 
+            // ChatTextBox
+            // 
+            this.ChatTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.ChatTextBox.Location = new System.Drawing.Point(20, 352);
+            this.ChatTextBox.Multiline = true;
+            this.ChatTextBox.Name = "ChatTextBox";
+            this.ChatTextBox.Size = new System.Drawing.Size(454, 106);
+            this.ChatTextBox.TabIndex = 8;
+            // 
+            // SendingMessageTextBox
+            // 
+            this.SendingMessageTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.SendingMessageTextBox.Location = new System.Drawing.Point(20, 464);
+            this.SendingMessageTextBox.Multiline = true;
+            this.SendingMessageTextBox.Name = "SendingMessageTextBox";
+            this.SendingMessageTextBox.Size = new System.Drawing.Size(381, 39);
+            this.SendingMessageTextBox.TabIndex = 9;
+            // 
+            // ConnectButton
+            // 
+            this.ConnectButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.ConnectButton.Location = new System.Drawing.Point(494, 445);
+            this.ConnectButton.Name = "ConnectButton";
+            this.ConnectButton.Size = new System.Drawing.Size(131, 57);
+            this.ConnectButton.TabIndex = 10;
+            this.ConnectButton.Text = "Connect";
+            this.ConnectButton.UseVisualStyleBackColor = true;
+            this.ConnectButton.Click += new System.EventHandler(this.ConnectButton_Click);
+            // 
+            // SendMessageButton
+            // 
+            this.SendMessageButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.SendMessageButton.Location = new System.Drawing.Point(407, 464);
+            this.SendMessageButton.Name = "SendMessageButton";
+            this.SendMessageButton.Size = new System.Drawing.Size(67, 39);
+            this.SendMessageButton.TabIndex = 11;
+            this.SendMessageButton.Text = "Send";
+            this.SendMessageButton.UseVisualStyleBackColor = true;
+            // 
+            // VideoDevicesComboBox
+            // 
+            this.VideoDevicesComboBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.VideoDevicesComboBox.FormattingEnabled = true;
+            this.VideoDevicesComboBox.Location = new System.Drawing.Point(494, 354);
+            this.VideoDevicesComboBox.Name = "VideoDevicesComboBox";
+            this.VideoDevicesComboBox.Size = new System.Drawing.Size(131, 24);
+            this.VideoDevicesComboBox.TabIndex = 12;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(491, 331);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(141, 20);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "Available cameras:";
+            // 
+            // StartCameraButton
+            // 
+            this.StartCameraButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.StartCameraButton.Location = new System.Drawing.Point(494, 384);
+            this.StartCameraButton.Name = "StartCameraButton";
+            this.StartCameraButton.Size = new System.Drawing.Size(131, 33);
+            this.StartCameraButton.TabIndex = 14;
+            this.StartCameraButton.Text = "Turn on camera";
+            this.StartCameraButton.UseVisualStyleBackColor = true;
+            this.StartCameraButton.Click += new System.EventHandler(this.StartCameraButton_Click);
+            // 
+            // Form1
+            // 
+            this.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.ClientSize = new System.Drawing.Size(641, 515);
+            this.Controls.Add(this.StartCameraButton);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.VideoDevicesComboBox);
+            this.Controls.Add(this.SendMessageButton);
+            this.Controls.Add(this.ConnectButton);
+            this.Controls.Add(this.SendingMessageTextBox);
+            this.Controls.Add(this.ChatTextBox);
+            this.Controls.Add(this.RemotePortTextBox);
+            this.Controls.Add(this.LocalPortTextBox);
+            this.Controls.Add(this.IPAddressTextBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.MyPictureBox);
+            this.Controls.Add(this.friendsPictureBox);
+            this.Name = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.friendsPictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.MyPictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private PictureBox friendsPictureBox;
+        private PictureBox MyPictureBox;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private TextBox IPAddressTextBox;
+        private TextBox LocalPortTextBox;
+        private TextBox RemotePortTextBox;
+        private TextBox ChatTextBox;
+        private TextBox SendingMessageTextBox;
+        private Button ConnectButton;
+        private Button SendMessageButton;
+        private ComboBox VideoDevicesComboBox;
+        private Label label4;
+        private Button StartCameraButton;
+    }
+}
+
---
+++ Cs- web programming/homeworks/hw1_25.01.18/hw1_25.01.18_new/Form1.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using AForge.Video;
+using AForge.Video.DirectShow;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace hw1_25._01._18_new
+{
+    public partial class Form1 : Form
+    {
+        private FilterInfoCollection videoDevices;
+        private VideoCaptureDevice activeVideoDevice;
+        private UdpClient sendingUdpClient;
+        private IPEndPoint sendingEndPoint;
+        private UdpClient receivingUdpClient;
+        private Bitmap currentBitmap;
+        private Thread receivingThread;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            videoDevices = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+            foreach(FilterInfo device in videoDevices)
+                VideoDevicesComboBox.Items.Add(device.Name);
+            VideoDevicesComboBox.SelectedIndex = 0;
+        }
+
+        private void StartCameraButton_Click(object sender, EventArgs e)
+        {
+            if (StartCameraButton.Text == "Turn on camera")
+            {
+                activeVideoDevice = new VideoCaptureDevice(videoDevices[VideoDevicesComboBox.SelectedIndex].MonikerString);
+                activeVideoDevice.VideoResolution = activeVideoDevice.VideoCapabilities[3];
+                activeVideoDevice.NewFrame += UpdateMyPictyreBox;
+                activeVideoDevice.Start();
+ 
+                StartCameraButton.Text = "Turn of camera";
+            }
+            else
+            {
+                activeVideoDevice.NewFrame -= UpdateMyPictyreBox;
+                activeVideoDevice.Stop();
+
+                MyPictureBox.Image = null;
+                MyPictureBox.Invalidate();
+
+                StartCameraButton.Text = "Turn on camera";
+            }
+        }
+
+        private void UpdateMyPictyreBox(object sender, NewFrameEventArgs eventArgs)
+        {
+            var oldBitmap = currentBitmap;     
+            currentBitmap = (Bitmap)eventArgs.Frame.Clone();
+            MyPictureBox.Image = currentBitmap;
+            oldBitmap?.Dispose();
+
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            activeVideoDevice?.SignalToStop();
+            receivingUdpClient?.Close();
+        }
+
+        private void ConnectButton_Click(object sender, EventArgs e)
+        {
+            if (ConnectButton.Text == "Connect")
+            {
+                sendingUdpClient = new UdpClient();
+
+                sendingEndPoint = new IPEndPoint(IPAddress.Parse(IPAddressTextBox.Text), Int32.Parse(RemotePortTextBox.Text));
+                receivingUdpClient = new UdpClient(Int32.Parse(LocalPortTextBox.Text));
+                //sendingEndPoint = new IPEndPoint(IPAddress.Parse("192.168.0.103"), 6000);
+                //receivingUdpClient = new UdpClient(9000);
+
+                if(activeVideoDevice != null && activeVideoDevice.IsRunning == true)
+                    activeVideoDevice.NewFrame += SendVideoBitmap;
+
+                receivingThread = new Thread(new ThreadStart(ReceivingVideoBitmap));
+                receivingThread.Start();
+
+                ConnectButton.Text = "Disconnect";
+            }
+            else
+            {
+                receivingThread?.Abort();
+                sendingUdpClient?.Close();
+                receivingUdpClient?.Close();
+
+                friendsPictureBox.Image = null;
+                friendsPictureBox.Invalidate();
+
+                if(activeVideoDevice != null)
+                    activeVideoDevice.NewFrame -= SendVideoBitmap;
+
+                ConnectButton.Text = "Connect";
+            }
+        }
+
+        private void SendVideoBitmap(object sender, NewFrameEventArgs eventArgs)
+        {
+            try
+            {
+                using (var stream = new MemoryStream())
+                {
+                    var picture = eventArgs.Frame;
+                    picture.Save(stream, System.Drawing.Imaging.ImageFormat.Png);
+                    byte[] bitMapArray = stream.ToArray();
+                    byte[] packageToSend = new byte[1024];
+
+                    int packageCounter = 0;
+                    for (int i = 0; i < bitMapArray.Length - 1000; i += 1000)
+                    {
+                        byte[] buff = Encoding.UTF8.GetBytes( packageCounter.ToString().ToArray());
+                        Array.Copy(bitMapArray, i, packageToSend, 0, 1000);
+                        buff.CopyTo(packageToSend, 1000);
+                        sendingUdpClient?.Send(packageToSend, 1024, sendingEndPoint);
+
+                        packageCounter++;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+            }
+        }
+
+        private void ReceivingVideoBitmap()
+        {
+            IPEndPoint RemoteIpEndPoint = null;
+            try
+            {
+                while (true)
+                {
+                    int lastPackage = 0;
+                    int currentPackage = 0;
+                    using (var stream = new MemoryStream())
+                    {
+                        while (currentPackage >= lastPackage)
+                        {
+                            lastPackage = currentPackage;
+                            byte[] receiveBytes = receivingUdpClient.Receive(ref RemoteIpEndPoint);
+                            currentPackage = Convert.ToInt32(Encoding.UTF8.GetString(receiveBytes, 1000, 24));
+                            stream.WriteAsync(receiveBytes, 0, 1000);
+                        }
+                        friendsPictureBox.Image = new Bitmap(stream);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+            }
+        }
+    }
+}
